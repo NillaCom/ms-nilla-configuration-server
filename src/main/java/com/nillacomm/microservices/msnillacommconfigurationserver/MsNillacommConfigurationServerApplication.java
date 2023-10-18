c7421b4bed2ab8a41a937f3603b5f69d881c0ac8
Robert Njiriri Kagema <rnjiriri@safaricom.co.ke>
2023-10-18 20:39:03 +0300

    initialize project

---
+++ src/main/java/com/nillacomm/microservices/msnillacommconfigurationserver/MsNillacommConfigurationServerApplication.java
@@ -0,0 +1,17 @@
+package com.nillacomm.microservices.msnillacommconfigurationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class MsNillacommConfigurationServerApplication {
+
+	public static void main(String[] args) {
+		var springApplication = new SpringApplication(MsNillacommConfigurationServerApplication.class);
+		springApplication.setAddCommandLineProperties(false);
+		springApplication.run();
+	}
+
+}
